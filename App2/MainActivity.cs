cd0e3d640c35856edda6a2085c167abb642d047a
Pedro Vitor <Pedro Vitor>
2017-02-08 23:47:14 -0200

    First Commit

---
+++ App2/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App2
+{
+    [Activity(Label = "App2", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int countClick = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+
+
+            Button b = FindViewById<Button>(Resource.Id.button1);
+            b.Click += delegate { b.Text = string.Format("{0} clicks!", countClick )
+                
+            }
+    }
+}
+
